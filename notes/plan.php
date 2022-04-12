04582646521c80e03fb421ef49c074afb9c5bee1
m-tsi <m.i.tsirakidou@gmail.com>
2022-04-12 19:25:35 +0200

    singleplayer

---
+++ notes/plan.php
@@ -0,0 +1,31 @@
+<? php
+
+
+#
+#gets: number of active dice (int), number of turns left (int)
+#returns: set of 0-5 random numbers row1 (array int(0-6))
+function rolldice($dice_amount,$turns){
+  #...
+}
+
+#
+#gets: set of 0-5 numbers row2, clicked field in table
+#returns: calculates points and puts number in table
+function calculatepoints(){
+  #... 
+}
+
+
+#
+#gets: clicked field in row2
+#returns:
+function
+
+
+#
+#gets: meowm
+#returns:
+function
+
+
+?>
